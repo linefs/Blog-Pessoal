41afe40924f2204dfbe32bfd844004085a1b7c67
Aline Freitas <148965353+linefs@users.noreply.github.com>
2024-02-27 13:59:48 -0300

    Update PostagemController.java

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -26,7 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
     @Autowired
